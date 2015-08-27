a2e7c93ad1fe2936bbf2c50a1272f9e80198ca34
Douglas Bastos <douglashsb@gmail.com>
2015-08-27 18:58:41 -0300

    Cria arquivo setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from setuptools import find_packages, setup
+
+setup(
+    name='ranking_real_time',
+    description='None',
+    author='Douglas Bastos',
+    author_email='douglashsb@gmail.com',
+    url='https://github.com/douglasbastos/ranking_real_time',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[]
+)
