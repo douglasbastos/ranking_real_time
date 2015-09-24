cf03d873f664b2081d7a697c9b0a09ceed62a0d0
Douglas Bastos <douglashsb@gmail.com>
2015-09-24 02:13:33 -0300

    Cria rota de urls

---
+++ ranking_real_time/urls.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+from django.conf.urls import url
+
+from .views import mysql, redis
+
+urlpatterns = [
+    url(r'mysql/(?P<qnt>[0-9]+)/', mysql, name='ranking_com_mysql'),
+    url(r'redis/(?P<qnt>[0-9]+)/', redis, name='ranking_com_redis'),
+]   
