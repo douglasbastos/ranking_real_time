ffc432eaf65f5d42bfbd014f1742f2e3f253cd64
Douglas Bastos <douglashsb@gmail.com>
2015-11-04 19:32:30 -0200

    Define tempo que script vai ficar rodando

--- ranking_real_time/management/commands/jogar.py
+++ ranking_real_time/management/commands/jogar.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-
+import time
 from random import choice
 from django.core.management.base import BaseCommand
 from ranking_real_time.models import Player
@@ -14,6 +14,10 @@ cache = redis.StrictRedis(host=settings.REDIS_DB['host'],
 class Command(BaseCommand):
     help = 'Insere pontos dos usuários no mysql'
 
+    def add_arguments(self, parser):
+        parser.add_argument('-t', dest='time',
+                            default=30, help='Set time script to run')
+
     def jogar_partida(self, pontos):
         result = choice(('winner', 'loser'))
         pontos_ganhos = 25 if result == 'winner' else -25
@@ -25,9 +29,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         players = Player.objects.all()
-        while True:
+        start = now = int(time.time())
+        duration = int(options['time'])
+        limit = start + duration
+        while True and now <= limit:
             player_sorteado = choice(players)
             player_sorteado.pontos = self.jogar_partida(player_sorteado.pontos)
             player_sorteado.save()
             cache.zadd('ranking:username', player_sorteado.pontos, player_sorteado.username)
+            now = int(time.time())
             print 'Jogador {} está com {} pontos'.format(player_sorteado.username, player_sorteado.pontos)
